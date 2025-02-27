8b51d76c92c34fc3da59c1f7e53f3f6eb8052216
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-02-27 19:53:20 +0300

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+int main() {
+     string namee;
+     cout << "What is your name?"<< endl;
+     cin >> namee ;
+     cout << "Hello" << " " << namee << endl;
+     
+     int studentId;
+    cout << "What is your Student ID? " <<" " << endl;
+    cin >> studentId;   
+    cout << "Your ID is" << " "<< studentId << endl;
+    
+    
+  
+    
+    
+    
+    
+ 
+string name; 
+ 
+string surname;
+ 
+ 
+cout<<"What is your name? ";
+ 
+cin>>name;
+ 
+cout<<"What is your surname? ";
+ 
+cin>>surname;
+ 
+float lab_grade; 
+ 
+float midterm_grade;
+ 
+float final_grade;
+ 
+cout<<"What is your lab grade? ";
+ 
+cin>>lab_grade;
+ 
+cout<<"What is your midterm grade? ";
+ 
+cin>>midterm_grade;
+ 
+cout<<"What is your final grade? ";
+ 
+cin>>final_grade;
+ 
+float last_score = (lab_grade * 25 / 100) + (midterm_grade * 35 / 100) + (final_grade * 40 / 100);
+ 
+cout<<"Name: "<<name + " " + surname<<endl;
+ 
+cout<<"Lab: "<<lab_grade<<endl;
+ 
+cout<<"Midterm: "<<midterm_grade<<endl;
+ 
+cout<<"Final: "<<final_grade<<endl;
+ 
+cout<<"Last score: "<<last_score<<endl;
+ 
+
+ 
+
+
+ 
+cout<<"*\n**\n***\n**\n*"<<endl;
+ 
+return 0;
+
+    
+    
+   
+    
+}
